20984b7063d63a743a1f113e20c65459ddeec30c
Dominic Galarce <dgalarce@mobdev.cl>
2022-11-25 10:35:17 -0300

    implementacion controller

--- src/infrastructure/controller/pokemonController.ts
+++ src/infrastructure/controller/pokemonController.ts
@@ -1,12 +1,18 @@
 import { IGetAllPokemon } from "../../domain/port/input/IGetAllPokemon";
-import AllPokemonToAllPokemonResponse from "./mapper/AllPokemonToAllPokemonResponseMapper";
+import { IGetPokemonById } from "../../domain/port/input/IGetPokemonById";
+import AllPokemonToAllPokemonResponse from "./mapper/allPokemonToAllPokemonResponseMapper";
 
 export class PokemonController {
-  constructor(private readonly IGetAllPokemon: IGetAllPokemon) {
+  constructor(private readonly IGetAllPokemon: IGetAllPokemon, private readonly IGetPokemonById: IGetPokemonById ) {
     this.getAllPokemon = this.getAllPokemon.bind(this);
+    this.getPokemonById = this.getPokemonById.bind(this);
   }
 
   async getAllPokemon() {
     return AllPokemonToAllPokemonResponse(await this.IGetAllPokemon.execute());
+  }
+
+  async getPokemonById(id : number) {
+    return await this.IGetPokemonById.execute(id);
   }
 }
