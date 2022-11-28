b64bb73b578c2b76fea392a29a5bb1b882a6dc1f
MarceloVasquez662 <marcelovasquez662@gmail.com>
2022-11-28 12:15:47 -0300

    CSS personalizado para evitar errores

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import swaggerDocument from "./resources/swagger.json";
 import pokemonRouter from "./infrastructure/routes/pokemonRouter";
 
 const app = express();
-
 app.use(pokemonRouter);
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const swaggerOptions = {
+  customCssUrl:
+    "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-flattop.css",
+};
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 /*Decirle a express que inicialize el servidor en el puerto 5000 y que responda con un 
 mensaje si todo sale bien*/
