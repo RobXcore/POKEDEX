9132987a3d7e5d30520de52a02059b274719aba8
itorres6 <itorresv97@gmail.com>
2022-11-28 15:15:52 -0300

    completa 1era versión de GetPokemonByTypeUseCase

--- src/domain/usecase/GetPokemonByTypeUseCase.ts
+++ src/domain/usecase/GetPokemonByTypeUseCase.ts
@@ -1,13 +1,23 @@
 import { Pokemon } from "../model/IPokemon";
 import { IGetPokemonByType } from "../port/input/IGetPokemonByType";
-
+import { IGetPokemonByTypeGateway } from "../port/output/IGetPokemonByTypeGateway";
+import { IGetPokemonByUrlGateway } from "../port/output/IGetPokemonByUrlGateway";
 
 export class GetPokemonByTypeUseCase implements IGetPokemonByType {
-    
-    // TODO: implementar caso de uso
-    
-    execute(type: string): Promise<Pokemon[]> {
-        throw new Error("Method not implemented.");
-    }
+  constructor(
+    private readonly IGetPokemonByTypeGateway: IGetPokemonByTypeGateway,
+    private readonly IGetPokemonByUrlGateway: IGetPokemonByUrlGateway
+  ) {}
+  // TODO: implementar caso de uso
 
+  async execute(type: string | number): Promise<Pokemon[]> {
+    const pokemonType = await this.IGetPokemonByTypeGateway.execute(type);
+    const typePokemon = await Promise.all(
+      pokemonType.pokemon.map(
+        async (pokemon) =>
+          await this.IGetPokemonByUrlGateway.execute(pokemon.url)
+      )
+    );
+    return typePokemon;
+  }
 }
