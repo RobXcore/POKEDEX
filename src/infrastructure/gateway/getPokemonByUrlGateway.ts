9c75f954dcaa8990e9430730be8e2aea7bf0f0cf
MarceloVasquez662 <marcelovasquez662@gmail.com>
2022-11-29 15:55:03 -0300

    Fetch centralizado + nuevo mapper

--- src/infrastructure/gateway/getPokemonByUrlGateway.ts
+++ src/infrastructure/gateway/getPokemonByUrlGateway.ts
@@ -1,23 +1,14 @@
 import { Pokemon } from "../../domain/model/IPokemon";
 import { IGetPokemonByUrlGateway } from "../../domain/port/output/IGetPokemonByUrlGateway";
-import { ApiErrorException } from "../exception/ApiErrorException";
 import PokemonApiToPokemonMapper from "./mapper/pokemonApiToPokemonMapper";
 import { PokemonApi } from "./model/IPokemonApi";
-
-const ERROR_MESSAGE = "La respuesta de la API fue distinta de 200: ";
-const INTERNAL_SERVER_ERROR = 500;
+import FetchClientUtil from "./util/FetchClientUtil";
 
 export class GetPokemonByUrlGateway implements IGetPokemonByUrlGateway {
   async execute(url: string): Promise<Pokemon> {
-    const responseApi = await fetch(url);
-    if (responseApi.status === 200) {
-      const pokemonApi = (await responseApi.json()) as PokemonApi;
-      return PokemonApiToPokemonMapper(pokemonApi);
-    } else {
-      throw new ApiErrorException(
-        ERROR_MESSAGE + responseApi.url + responseApi.status + responseApi.statusText,
-        INTERNAL_SERVER_ERROR
-      );
-    }
+    const responseApi = await FetchClientUtil.get(url);
+    const pokemonApi = responseApi as PokemonApi;
+
+    return PokemonApiToPokemonMapper(pokemonApi);
   }
 }
