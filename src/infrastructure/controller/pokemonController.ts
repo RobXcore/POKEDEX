aca1ae0d2faeeb469169d64990e132f74eae00f3
itorres6 <itorresv97@gmail.com>
2022-11-29 15:37:57 -0300

    añade dependencia y método getPokemonByType en pokemonController

--- src/infrastructure/controller/pokemonController.ts
+++ src/infrastructure/controller/pokemonController.ts
@@ -1,14 +1,23 @@
 import { IGetAllPokemon } from "../../domain/port/input/IGetAllPokemon";
 import AllPokemonToAllPokemonResponse from "./mapper/allPokemonToAllPokemonResponseMapper";
+import { IGetPokemonByType } from "../../domain/port/input/IGetPokemonByType";
 
 export class PokemonController {
-  constructor(private readonly IGetAllPokemon: IGetAllPokemon) {
+  constructor(
+    private readonly IGetAllPokemon: IGetAllPokemon,
+    private readonly IGetPokemonByType: IGetPokemonByType
+  ) {
     this.getAllPokemon = this.getAllPokemon.bind(this);
+    this.getPokemonByType = this.getPokemonByType.bind(this);
   }
 
   async getAllPokemon(offset: number) {
     return AllPokemonToAllPokemonResponse(
       await this.IGetAllPokemon.execute(offset)
     );
+  }
+
+  async getPokemonByType(type: string | number) {
+    await this.IGetPokemonByType.execute(type);
   }
 }
