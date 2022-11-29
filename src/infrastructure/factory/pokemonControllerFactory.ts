378a421553e056a9eb3d54a168f0c491b7d85db4
itorres6 <itorresv97@gmail.com>
2022-11-29 15:38:34 -0300

    inicializa gateway getPokemonType y caso de uso getPokemonByType en pokemonControllerFactory

--- src/infrastructure/factory/pokemonControllerFactory.ts
+++ src/infrastructure/factory/pokemonControllerFactory.ts
@@ -2,12 +2,19 @@ import { GetAllPokemonUseCase } from "../../domain/usecase/getAllPokemonUseCase"
 import { GetPokemonListGateway } from "../gateway/getPokemonListGateway";
 import { GetPokemonByUrlGateway } from "../gateway/getPokemonByUrlGateway";
 import { PokemonController } from "../controller/pokemonController";
+import { GetPokemonByTypeUseCase } from '../../domain/usecase/GetPokemonByTypeUseCase';
+import { GetPokemonTypeGateway } from '../gateway/GetPokemonTypeGateway';
 
 const getPokemonListGateway = new GetPokemonListGateway();
 const getPokemonByUrlGateway = new GetPokemonByUrlGateway();
+const getPokemonTypeGateway = new GetPokemonTypeGateway();
 const getAllPokemonUseCase = new GetAllPokemonUseCase(
   getPokemonListGateway,
   getPokemonByUrlGateway
 );
+const getPokemonByTypeUseCase = new GetPokemonByTypeUseCase(
+  getPokemonTypeGateway,
+  getPokemonByUrlGateway
+)
 
-export const pokemonController = new PokemonController(getAllPokemonUseCase);
+export const pokemonController = new PokemonController(getAllPokemonUseCase, getPokemonByTypeUseCase);
