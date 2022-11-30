1155d7c5d4888dca4d299cdbd8b13ad72080aa92
Dominic Galarce <dgalarce@mobdev.cl>
2022-11-30 16:26:27 -0300

    se agrega uso de clase utilitaria en gateway

--- src/infrastructure/gateway/getPokemonByIdGateway.ts
+++ src/infrastructure/gateway/getPokemonByIdGateway.ts
@@ -2,13 +2,14 @@ import { Pokemon } from "../../domain/model/IPokemon";
 import { IGetPokemonByIdGateway } from "../../domain/port/output/IGetPokemonByIdGateway";
 import PokemonApiToPokemonMapper from "./mapper/pokemonApiToPokemonMapper";
 import { PokemonApi } from "./model/IPokemonApi";
+import FetchClientUtil from "./util/FetchClientUtil";
 
 export class GetPokemonByIdGateway implements IGetPokemonByIdGateway {
   
     URL_GET_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
 
     async execute(id: number): Promise<Pokemon> {
-      const responseApi = await fetch(this.URL_GET_POKEMON+id);
+      const responseApi = await FetchClientUtil.get(this.URL_GET_POKEMON+id);
       const pokemonApi = (await responseApi.json()) as PokemonApi;
   
       return PokemonApiToPokemonMapper(pokemonApi);
