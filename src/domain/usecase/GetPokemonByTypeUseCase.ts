88eff8ebb5a706913d4b06dfb335201e729af747
itorres6 <itorresv97@gmail.com>
2022-11-30 18:31:41 -0300

    mueve lógica de validación desde pokemonController a GetPokemonByTypeUseCase

--- src/domain/usecase/GetPokemonByTypeUseCase.ts
+++ src/domain/usecase/GetPokemonByTypeUseCase.ts
@@ -3,6 +3,12 @@ import { Pokemon, NameUrl } from "../model/IPokemon";
 import { IGetPokemonByType } from "../port/input/IGetPokemonByType";
 import { IGetPokemonTypeGateway as IGetPokemonTypeGateway } from "../port/output/IGetPokemonTypeGateway";
 import { IGetPokemonByUrlGateway } from "../port/output/IGetPokemonByUrlGateway";
+import { RequestParamException } from "../../infrastructure/exception/RequestParamException";
+
+const NUMBER_OF_POKEMON_TYPES = 20;
+const BAD_REQUEST_STATUS_CODE = 400;
+const INVALID_TYPE_ID_IN_PATH_ERROR_MESSAGE =
+  "Si elige buscar por id del tipo, éste número entero debe encontrarse entre 1 y 20 inclusive.";
 
 export class GetPokemonByTypeUseCase implements IGetPokemonByType {
   constructor(
@@ -11,15 +17,27 @@ export class GetPokemonByTypeUseCase implements IGetPokemonByType {
   ) {}
 
   async execute(typeNameOrId: string): Promise<Pokemon[]> {
-    const pokemonType: TypeApi = await this.IGetPokemonTypeGateway.execute(
-      typeNameOrId
-    );
-    const typePokemon: Pokemon[] = await Promise.all(
-      pokemonType.pokemon.map(
-        async (pokemon: NameUrl) =>
-          await this.IGetPokemonByUrlGateway.execute(pokemon.url)
-      )
-    );
-    return typePokemon;
+    //Verifica si el valor recibido son letras o un número entre 0 y 20
+    if (
+      isNaN(+typeNameOrId) ||
+      +typeNameOrId >= 1 ||
+      +typeNameOrId <= NUMBER_OF_POKEMON_TYPES
+    ) {
+      const pokemonType: TypeApi = await this.IGetPokemonTypeGateway.execute(
+        typeNameOrId
+      );
+      const typePokemon: Pokemon[] = await Promise.all(
+        pokemonType.pokemon.map(
+          async (pokemon: NameUrl) =>
+            await this.IGetPokemonByUrlGateway.execute(pokemon.url)
+        )
+      );
+      return typePokemon;
+    } else {
+      throw new RequestParamException(
+        INVALID_TYPE_ID_IN_PATH_ERROR_MESSAGE,
+        BAD_REQUEST_STATUS_CODE
+      );
+    }
   }
 }
--- src/infrastructure/controller/pokemonController.ts
+++ src/infrastructure/controller/pokemonController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { IGetAllPokemon } from "../../domain/port/input/IGetAllPokemon";
-import { RequestParamException } from "../exception/RequstParamException";
+import { RequestParamException } from "../exception/RequestParamException";
 import AllPokemonToAllPokemonResponse from "./mapper/allPokemonToAllPokemonResponseMapper";
 import { IGetPokemonByType } from "../../domain/port/input/IGetPokemonByType";
 import PokemonListToPokemonByTypeResponse from "./mapper/PokemonListToPokemonByTypeResponseMapper";
@@ -9,8 +9,6 @@ const BAD_REQUEST_STATUS_CODE = 400;
 const INVALID_OFFSET_ERROR_MESSAGE = "El offset enviado no es numérico";
 const MISSING_TYPE_IN_PATH_ERROR_MESSAGE =
   "Debe indicarse en el path un nombre de tipo en inglés o algún número entero como id.";
-const INVALID_TYPE_ID_IN_PATH_ERROR_MESSAGE =
-  "Si elige buscar por id del tipo, éste número entero debe encontrarse entre 1 y 20 inclusive.";
 
 export class PokemonController {
   constructor(
@@ -43,7 +41,6 @@ export class PokemonController {
   }
 
   async getPokemonByType(req: Request, res: Response): Promise<void> {
-    const NUMBER_OF_POKEMON_TYPES = 20;
     let typeNameOrId: string;
 
     if (!req.params.type) {
@@ -55,22 +52,10 @@ export class PokemonController {
       typeNameOrId = req.params.type.trim();
     }
 
-    //Verifica si el valor ingresado son letras o un número entre 0 y 20
-    if (
-      isNaN(+typeNameOrId) ||
-      +typeNameOrId >= 1 ||
-      +typeNameOrId <= NUMBER_OF_POKEMON_TYPES
-    ) {
-      res.send(
-        PokemonListToPokemonByTypeResponse(
-          await this.IGetPokemonByType.execute(typeNameOrId)
-        )
-      );
-    } else {
-      throw new RequestParamException(
-        INVALID_TYPE_ID_IN_PATH_ERROR_MESSAGE,
-        BAD_REQUEST_STATUS_CODE
-      );
-    }
+    res.send(
+      PokemonListToPokemonByTypeResponse(
+        await this.IGetPokemonByType.execute(typeNameOrId)
+      )
+    );
   }
 }
