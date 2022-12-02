3b61e72c143fc6bd97ffde9aee2607009eacfc71
Dominic Galarce <dgalarce@mobdev.cl>
2022-12-02 11:47:37 -0300

    cambio variable gateway

--- src/infrastructure/gateway/getPokemonByIdGateway.ts
+++ src/infrastructure/gateway/getPokemonByIdGateway.ts
@@ -4,14 +4,13 @@ import PokemonApiToPokemonMapper from "./mapper/pokemonApiToPokemonMapper";
 import { PokemonApi } from "./model/IPokemonApi";
 import FetchClientUtil from "./util/FetchClientUtil";
 
-export class GetPokemonByIdGateway implements IGetPokemonByIdGateway {
-  
-    URL_GET_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+const URL_GET_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
 
-    async execute(id: number): Promise<Pokemon> {
-      const responseApi = await FetchClientUtil.get(this.URL_GET_POKEMON+id);
-      const pokemonApi = responseApi as PokemonApi;
-  
-      return PokemonApiToPokemonMapper(pokemonApi);
-    }
+export class GetPokemonByIdGateway implements IGetPokemonByIdGateway {
+  async execute(id: number): Promise<Pokemon> {
+    const responseApi = await FetchClientUtil.get(URL_GET_POKEMON + id);
+    const pokemonApi = responseApi as PokemonApi;
+
+    return PokemonApiToPokemonMapper(pokemonApi);
+  }
 }
