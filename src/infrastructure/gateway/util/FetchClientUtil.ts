8dea538a84967bc6a63c67d10685a16f133cd978
Dominic Galarce <dgalarce@mobdev.cl>
2022-12-02 11:54:23 -0300

    cambio clase utilitaria segun observaciones

--- src/infrastructure/gateway/util/FetchClientUtil.ts
+++ src/infrastructure/gateway/util/FetchClientUtil.ts
@@ -1,8 +1,8 @@
 import { ApiErrorException } from "../../exception/ApiErrorException";
 import { NotFoundException } from "../../exception/NotFoundException";
 
-const ERROR_MESSAGE_GET = "La respuesta de la API fue un status no controlado";
-const ERROR_MESSAGE_NOT_FOUND = "No encontrado ";
+const ERROR_MESSAGE_GET_FROM_API = "La respuesta de la API fue un status no controlado";
+const ERROR_MESSAGE_NOT_FOUND = "El siguiente recurso no fue encontrado: ";
 const INTERNAL_SERVER_ERROR = 500;
 const NOT_FOUND_ERROR = 404;
 
@@ -14,7 +14,7 @@ const get = async (url: string) => {
     throw new ApiErrorException(ERROR_MESSAGE_NOT_FOUND + `${responseApi.url}`, NOT_FOUND_ERROR);
   } else {
     throw new NotFoundException(
-      ERROR_MESSAGE_GET + `${responseApi.url} ${responseApi.status} ${responseApi.statusText}`,
+      ERROR_MESSAGE_GET_FROM_API + `${responseApi.url} ${responseApi.status} ${responseApi.statusText}`,
       INTERNAL_SERVER_ERROR
     );
   }
