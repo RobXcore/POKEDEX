e5b53b8f9d4527dd1b531b55f5431deb81686d20
itorres6 <itorresv97@gmail.com>
2022-12-02 12:43:37 -0300

    corrige definición para capturar datos de modelo gateway TypeApi

--- src/infrastructure/gateway/model/IPokemonTypeApi.ts
+++ src/infrastructure/gateway/model/IPokemonTypeApi.ts
@@ -3,5 +3,9 @@ import { NameUrl } from "../../../domain/model/IPokemon";
 export interface TypeApi {
   id: number;
   name: string;
-  pokemon: [pokemon: NameUrl];
+  pokemonList: PokemonObject[];
+}
+
+interface PokemonObject {
+  pokemon: NameUrl;
 }
