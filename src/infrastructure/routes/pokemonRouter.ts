e85324ac26b7eb10262d9f8e4356e86553bda995
Roberto Galindo <73141786+RobXcore@users.noreply.github.com>
2022-12-05 14:18:40 -0300

    route fix

--- src/infrastructure/routes/pokemonRouter.ts
+++ src/infrastructure/routes/pokemonRouter.ts
@@ -6,7 +6,7 @@ import { pokemonController } from "../factory/pokemonControllerFactory";
 const pokemonRouter = Router();
 
 pokemonRouter.get("/pokemon/all", Handler(pokemonController.getAllPokemon));
-pokemonRouter.get("/region/:region", Handler(controller.getPokemonByRegionController));
+pokemonRouter.get("/pokemon/region/:region", Handler(controller.getPokemonByRegionController));
 
 pokemonRouter.get("/pokemon/:id", Handler(pokemonController.getPokemonById));
 
