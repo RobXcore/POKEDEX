f0e8f77c9f12a77173f687e376a57b29a12eca43
MarceloVasquez662 <marcelovasquez662@gmail.com>
2022-12-07 14:22:48 -0300

    Clase responseValidator agregada

--- src/infrastructure/controller/pokemonController.ts
+++ src/infrastructure/controller/pokemonController.ts
@@ -6,13 +6,20 @@ import { IGetPokemonByType } from "../../domain/port/input/IGetPokemonByType";
 import AllPokemonToAllPokemonResponse from "./mapper/allPokemonToAllPokemonResponseMapper";
 import PokemonListToPokemonByTypeResponse from "./mapper/PokemonListToPokemonByTypeResponseMapper";
 import { IGetPokemonByRegion } from "../../domain/port/input/IGetPokemonByRegion";
+import { responseValidator } from "./util/ResponseValidator";
 
 /* Schemas and validators*/
 import { validate } from "jsonschema";
+/* Request schemas */
 import pokemonIdSchema from "./schemas/request/pokemonIdSchema.json";
 import pokemonTypeSchema from "./schemas/request/pokemonTypeSchema.json";
 import offsetSchema from "./schemas/request/offsetSchema.json";
 import pokemonRegionSchema from "./schemas/request/pokemonRegionSchema.json";
+/* Response schemas*/
+import allPokemonResponseSchema from "./schemas/response/allPokemonResponseSchema.json";
+import pokemonResponseSchema from "./schemas/response/pokemonResponseSchema.json";
+import pokemonListByTypeResponseSchema from "./schemas/response/pokemonListByTypeResponseSchema.json";
+import pokemonListByRegionResponseSchema from "./schemas/response/pokemonListByRegionResponseSchema.json";
 
 const BAD_REQUEST_STATUS_CODE = 400;
 const INVALID_REGION_ERROR_MESSAGE = "La región ingresada no existe en el universo Pokemon";
@@ -20,6 +27,8 @@ const INVALID_OFFSET_ERROR_MESSAGE = "El offset enviado no es numérico";
 const INVALID_ID_ERROR_MESSAGE = "El id ingresado no es válido";
 const MISSING_TYPE_IN_PATH_ERROR_MESSAGE =
   "Debe indicarse en el path un nombre de tipo en inglés o algún número entero como id entre 1 y 18 inclusive.";
+
+let validation;
 
 export class PokemonController {
   constructor(
@@ -36,46 +45,49 @@ export class PokemonController {
 
   async getAllPokemon(req: Request, res: Response): Promise<void> {
     const offset = Number(req.query.offset) || 0;
-    const validation = validate(offset, offsetSchema);
+    validation = validate(offset, offsetSchema);
 
     if (!validation.valid) {
       throw new RequestParamException(INVALID_OFFSET_ERROR_MESSAGE, BAD_REQUEST_STATUS_CODE);
     } else {
-      res.send(AllPokemonToAllPokemonResponse(await this.IGetAllPokemon.execute(offset)));
+      const allPokemon = AllPokemonToAllPokemonResponse(await this.IGetAllPokemon.execute(offset));
+      res.send(responseValidator(allPokemon, allPokemonResponseSchema));
     }
   }
 
   async getPokemonById(req: Request, res: Response): Promise<void> {
     const pokemonId = +req.params.id;
-    const validation = validate(pokemonId, pokemonIdSchema);
+    validation = validate(pokemonId, pokemonIdSchema);
 
     if (!validation.valid) {
       throw new RequestParamException(INVALID_ID_ERROR_MESSAGE, BAD_REQUEST_STATUS_CODE);
     } else {
-      res.send(await this.IGetPokemonById.execute(pokemonId));
+      const pokemon = await this.IGetPokemonById.execute(pokemonId);
+      res.send(responseValidator(pokemon, pokemonResponseSchema));
     }
   }
 
   async getPokemonByType(req: Request, res: Response): Promise<void> {
     const pokemonType = req.params.type;
-    const validation = validate(pokemonType, pokemonTypeSchema);
+    validation = validate(pokemonType, pokemonTypeSchema);
 
     if (!validation.valid) {
       throw new RequestParamException(MISSING_TYPE_IN_PATH_ERROR_MESSAGE, BAD_REQUEST_STATUS_CODE);
     } else {
-      res.send(PokemonListToPokemonByTypeResponse(await this.IGetPokemonByType.execute(pokemonType)));
+      const pokemonListByType = PokemonListToPokemonByTypeResponse(await this.IGetPokemonByType.execute(pokemonType));
+      res.send(responseValidator(pokemonListByType, pokemonListByTypeResponseSchema));
     }
   }
 
   async getPokemonByRegion(req: Request, res: Response): Promise<void> {
     const pokemonRegion = req.params.region;
-    const validation = validate(pokemonRegion, pokemonRegionSchema);
+    validation = validate(pokemonRegion, pokemonRegionSchema);
 
     if (!validation.valid) {
       throw new RequestParamException(INVALID_REGION_ERROR_MESSAGE, BAD_REQUEST_STATUS_CODE);
     } else {
-      const pokeRes = await this.IGetPokemonByRegion.execute(pokemonRegion);
-      res.send(pokeRes);
+      const pokemonListByRegion = await this.IGetPokemonByRegion.execute(pokemonRegion);
+      res.send(responseValidator(pokemonListByRegion, pokemonListByRegionResponseSchema));
     }
   }
 }
